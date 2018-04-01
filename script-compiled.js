04f41ffc8651e1051e116d6a76f633a6bcbb3b34
Wojtek Gumulski <wojciech.gumulski@gmail.com>
2018-04-01 23:14:22 +0200

    Add files

---
+++ script-compiled.js
@@ -0,0 +1,92 @@
+class Stopwatch {
+    constructor(display) {
+        this.running = false;
+        this.display = display;
+        this.reset();
+        this.print(this.times);
+    }
+    reset() {
+        this.times = {
+            minutes: 0,
+            seconds: 0,
+            miliseconds: 0
+        };
+    }
+
+    print() {
+        this.display.innerText = this.format(this.times);
+    }
+
+    format(times) {
+        return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
+    }
+
+    start() {
+        if (!this.running) {
+            this.running = true;
+            this.watch = setInterval(() => this.step(), 10);
+        }
+    }
+
+    step() {
+        if (!this.running) return;
+        this.calculate();
+        this.print();
+    }
+
+    calculate() {
+        this.times.miliseconds += 1;
+        if (this.times.miliseconds >= 100) {
+            this.times.seconds += 1;
+            this.times.miliseconds = 0;
+        }
+        if (this.times.seconds >= 60) {
+            this.times.minutes += 1;
+            this.times.seconds = 0;
+        }
+    }
+
+    stop() {
+        this.running = false;
+        clearInterval(this.watch);
+    }
+
+    results() {
+        let wynik = this.display.innerText = this.format(this.times);
+        let rezultaty = document.querySelector('.results');
+        let czas = document.createElement("li");
+        czas.innerText = wynik;
+        rezultaty.appendChild(czas);
+    }
+
+    clear() {
+        document.getElementById('results').innerHTML = "";
+    }
+}
+
+function pad0(value) {
+    let result = value.toString();
+    if (result.length < 2) {
+        result = '0' + result;
+    }
+    return result;
+}
+
+const stopwatch = new Stopwatch(document.querySelector('.stopwatch'));
+
+let startButton = document.getElementById('start');
+startButton.addEventListener('click', () => stopwatch.start());
+
+let stopButton = document.getElementById('stop');
+stopButton.addEventListener('click', () => stopwatch.stop());
+
+let resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', () => {
+    stopwatch.reset(), stopwatch.print();
+});
+
+let saveButton = document.getElementById('save');
+saveButton.addEventListener('click', () => stopwatch.results());
+
+let clearButton = document.getElementById('clear');
+clearButton.addEventListener('click', () => stopwatch.clear());
